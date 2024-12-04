3e8fbad8b8013eef1ef8ef6b691a7da090476a4c
helloghostt <lamar0610@gmail.com>
2024-12-04 20:40:15 +0900

    TestApp clone Kotlin, Android studio 인프런 "Code With Joyce"

---
+++ app/src/main/java/com/example/lovetest/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.lovetest
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.findNavController
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.my_nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+    }
+}
